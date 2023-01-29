014c64e3cbf9be751df3f57bb9335fe39601ab0f
mwyrwicki <53524654+mwyrwicki@users.noreply.github.com>
2023-01-29 18:31:03 +0100

    Update oop_tic_tac_toe.py

--- oop_tic_tac_toe.py
+++ oop_tic_tac_toe.py
@@ -161,3 +161,62 @@ class Board:
         self.game_board = [[0, 0, 0],
                            [0, 0, 0],
                            [0, 0, 0]]
+
+
+class Game:
+
+    def start(self):
+        print('==========================')
+        print('  Welcome to Tic-Tac-Toe  ')
+        print('==========================')
+
+        board = Board()
+        player = Player()
+        computer = Player(False)
+
+        board.print_board()
+
+        # Ask user if he/she would like to play another round
+        while True:
+            # Get move, check tie, check game over
+            while True:
+                player_move = player.get_move()
+                board.submit_move(player, player_move)
+                board.print_board()
+
+                if board.check_is_tie():
+                    print("It's a tie! Try again.")
+                    break
+                elif board.check_is_game_over(player, player_move):
+                    print("Awesome. You won the game!")
+                    break
+                else:
+                    computer_move = computer.get_move()
+                    board.submit_move(computer, computer_move)
+                    board.print_board()
+
+                    if board.check_is_game_over(computer, computer_move):
+                        print("Computer won. Try again.")
+                        break
+
+            play_again = input("Would you like to play again?"
+                               "Enter Y for YES or N for NO: ").upper()
+            if play_again == "N":
+                print("Bye! Comeback soon!")
+                break
+            elif play_again == "Y":
+                self.start_new_round(board)
+            else:
+                print("Your input was invalid, let's play again")
+                self.start_new_round(board)
+
+    def start_new_round(self, board):
+        print('==========================')
+        print('        New round         ')
+        print('==========================')
+        board.reset_board()
+        board.print_board()
+
+
+game = Game()
+game.start()
